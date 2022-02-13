1d8d129effe77fefe679a4dba0de3e848a200c1e
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-13 18:11:19 +0530

    Added : StopWatch

---
+++ src/com/blz/java/StopWatch.java
@@ -0,0 +1,28 @@
+package com.blz.java;
+
+import java.util.Scanner;
+
+public class StopWatch {
+	public static double startWatch() {
+		System.out.println("Enter 1 to start watch");
+		Scanner scanner = new Scanner(System.in);
+		double startWatch = scanner.nextDouble();
+		startWatch = System.currentTimeMillis();
+		return startWatch;
+
+	}
+
+	public static double stopWatch() {
+		System.out.println("Enter 0 to stop watch");
+		Scanner scanner = new Scanner(System.in);
+		double stopWatch = scanner.nextDouble();
+		stopWatch = System.currentTimeMillis();
+		return stopWatch;
+
+	}
+
+	public static void main(String[] args) {
+		double timeInSec = Math.abs((startWatch() - stopWatch())) / 1000;
+		System.out.println("time in sec is " + timeInSec);
+	}
+}
